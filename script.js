a2ce2fea61d58b211466eab85634eb0dae486879
Warren Santiago <wsantiago9623@gmail.com>
2022-09-13 13:38:33 -0400

    Added button

--- script.js
+++ script.js
@@ -3,4 +3,18 @@ h2.textContent = "This content added by JavaScript";
 
 document.querySelector("body").appendChild(h2);
 
-addEventListener
+//Add event listener that serves as an example of a functional button (needs two arguments, look up lesson)
+
+const btn = document.createElement("button")
+
+btn.innerHTML = "Try me!"
+
+document.body.appendChild(btn)
+
+btn.onclick = function (){
+    alert("I work!");
+}
+
+
+
+
